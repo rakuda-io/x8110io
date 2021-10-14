3977508c3b83f215925d5e41f3075cb6a555eae1
rakuda-io <rakudalms@gmail.com>
2021-10-15 08:28:11 +0900

    createアクションテスト異常パターン、updateアクションテスト正常・異常パターン

--- app/controllers/api/holdings_controller.rb
+++ app/controllers/api/holdings_controller.rb
@@ -7,28 +7,34 @@ module Api
 
     def index
       holdings = @user.holdings.order(created_at: :desc)
-      render status: :ok, json: holdings
+      render json: holdings, status: :ok
     end
 
     def show
       holding = @user.holdings.where(id: params[:id])
-      render status: :ok, json: holding
+      render json: holding, status: :ok
     end
 
     def create
       @new_holding = @user.holdings.new(holding_params)
+      # quantityが未入力なら後々の計算式がエラーになるので早期リターンさせる
+      if @new_holding.quantity == nil
+        return render json: {errors: "You must fill out the fields of Quantity"}, status: :unprocessable_entity
+
       # 同じstock_idが存在するなら追加・更新
-      if @new_holding.same_stock_id_exist?
+      elsif @new_holding.same_stock_id_exist?
         same_holding = @user.holdings.find_by(stock_id: @new_holding.stock_id)
         same_holding.update(quantity: same_holding.quantity += @new_holding.quantity)
-        render status: :ok, json: same_holding
+        render json: same_holding, status: :ok
 
       # 同じstock_idが存在しないなら新規登録
       elsif @new_holding.save
         fetch_current_dividend # 現在のdividendを取得
         render status: :created, json: @new_holding
+
       else
-        render status: :unprocessable_entity, json: "You must fill out the fields!!"
+        # その他の項目が未入力なら
+        render json: {errors: "You must fill out the fields!!"}, status: :unprocessable_entity
       end
     end
 
@@ -38,10 +44,9 @@ module Api
         holding.update(
           quantity: holding.quantity += holding_params[:quantity].to_f
         )
-        holding.fetch_current_dividend
         render status: :ok, json: holding
       else
-        render status: :unprocessable_entity, json: "You must fill out the fields!!"
+        render json: {errors: "You must fill out the fields!!"}, status: :unprocessable_entity
       end
     end
 
--- spec/requests/api/holdings_spec.rb
+++ spec/requests/api/holdings_spec.rb
@@ -15,13 +15,13 @@ RSpec.describe 'Holdings API', type: :request do
       let(:tokens) { sign_in(params) }
       let(:params) { { email: user[:email], password: 'password' } }
 
-      # 事前にログインしておく
+      # 事前にサインインしておく
       before { post 'http://localhost:3000/api/auth', params: params }
 
       context 'ユーザーAがユーザーAの保有株一覧を取得しようとした場合' do
         before { get "#{base_url}#{user[:id]}/holdings", headers: tokens }
 
-        it 'ログインしたユーザーの持っている保有株一覧が取得できること' do
+        it 'サインインしたユーザーの持っている保有株一覧が取得できること' do
           expect(JSON.parse(response.body)[0]['user']['name']).to eq(user[:name])
         end
 
@@ -63,13 +63,13 @@ RSpec.describe 'Holdings API', type: :request do
       let(:tokens) { sign_in(params) }
       let(:params) { { email: user[:email], password: 'password' } }
 
-      # 事前にログインしておく
+      # 事前にサインインしておく
       before { post 'http://localhost:3000/api/auth', params: params }
 
       context 'ユーザーAがユーザーAの保有株をひとつ取得しようとした場合' do
         before { get "#{base_url}#{user[:id]}/holdings/#{user.holdings.first[:id]}", headers: tokens }
 
-        it 'ログインしたユーザーの持っている保有株が取得できること' do
+        it 'サインインしたユーザーの持っている保有株が取得できること' do
           expect(JSON.parse(response.body)[0]['user']['id']).to eq(user[:id])
         end
 
@@ -107,49 +107,144 @@ RSpec.describe 'Holdings API', type: :request do
   end
 
   describe '#create Action' do
-    context '正常' do
-      let(:tokens) { sign_in(params) }
-      let(:params) { { email: user[:email], password: 'password' } }
-      # factory_botのstockを実際にスクレイピング出来るデータにオーバーライド
-      let!(:new_stock) do
-        create(:stock,
-               company_name: "SPDR Portfolio S&P 500 High Dividend ETF",
-               ticker_symbol: "SPYD",
-               country: "USA",
-               sector: "Financial",
-               url: "https://finviz.com/quote.ashx?t=SPYD&ty=c&p=d&b=1")
+    let(:tokens) { sign_in(params) }
+    let(:params) { { email: user[:email], password: 'password' } }
+    # factory_botのstockを実際にスクレイピング出来るデータにオーバーライド
+    let!(:new_stock) do
+      create(:stock,
+        company_name: "SPDR Portfolio S&P 500 High Dividend ETF",
+        ticker_symbol: "SPYD",
+        country: "USA",
+        sector: "Financial",
+        url: "https://finviz.com/quote.ashx?t=SPYD&ty=c&p=d&b=1")
       end
 
-      # 事前にログインしておく
+    context '正常' do
+      # サインイン
       before { post 'http://localhost:3000/api/auth', params: params }
 
-      context 'サインイン出来ている場合' do
+      context 'すべてのholding_paramsのリクエストが送信されているとき' do
         let(:holding_params) { { quantity: 1, user_id: user[:id], stock_id: new_stock[:id] } }
-        before { post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params }
 
-        it 'paramsで送信した通りの株を新規登録できること' do
+        it 'holding_paramsで送信した通りの株を新規登録できること' do
+          post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params
           expect(JSON.parse(response.body)['stock']['company_name']).to eq(new_stock[:company_name])
         end
 
         it '新規登録時に現在のdividend(配当金額)が登録出来ていること' do
+          post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params
           expect(JSON.parse(response.body)['dividend_amount']).not_to be_nil
         end
 
         it 'HTTPステータスが201であること' do
+          post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params
           expect(response).to have_http_status(:created)
         end
+
         # 成功のスタータスコードのテストよりも、新規登録によって件数が1件増えたことをテストするのもあり
-
-        # 作れなかった時
-        #
+        it '新規登録で1件のHoldingテーブルのレコードが増えること' do
+          expect{post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params}
+            .to change{Holding.all.count}.by(+1)
+        end
       end
-
-      context 'サインイン出来ていない場合'
     end
 
-    context '異常'
+    context '異常' do
+      context 'リクエストにstock_idが含まれていないとき' do
+        let(:holding_params) { { quantity: 1, user_id: user[:id], stock_id: nil } }
+
+        it '登録できないこと' do
+          post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params
+          expect(JSON.parse(response.body)['errors']).to eq('You must fill out the fields!!')
+        end
+
+        it 'HTTPステータスが422である(処理ができない)こと' do
+          post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context 'リクエストにquantityが含まれていないとき' do
+        let(:holding_params) { { quantity: nil, user_id: user[:id], stock_id: new_stock[:id] } }
+
+        it '登録できないこと' do
+          post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params
+          expect(JSON.parse(response.body)['errors']).to eq('You must fill out the fields of Quantity')
+        end
+
+        it 'HTTPステータスが422である(処理ができない)こと' do
+          post "#{base_url}#{user[:id]}/holdings", headers: tokens, params: holding_params
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context 'サインイン出来ていない場合' do
+        let(:holding_params) { { quantity: 1, user_id: user[:id], stock_id: new_stock[:id] } }
+        before { post "#{base_url}#{user[:id]}/holdings", params: holding_params }
+
+        it '登録できないこと' do
+          expect(JSON.parse(response.body)['errors']).to eq(["You need to sign in or sign up before continuing."])
+        end
+
+        it 'HTTPステータスが401である(アクセス権がない)こと' do
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
+    end
   end
 
-  describe '#update Action'
+  describe '#update Action' do
+    let(:tokens) { sign_in(params) }
+    let(:params) { { email: user[:email], password: 'password' } }
+
+    context '正常' do
+      # サインイン
+      before { post 'http://localhost:3000/api/auth', params: params }
+
+      let(:update_params) { { quantity: 2 } }
+
+      it 'update_paramsのquantity分だけholdingのquantityが増加していること' do
+        expect{put "#{base_url}#{user[:id]}/holdings/#{user.holdings.first[:id]}", headers: tokens, params: update_params}
+        .to change{user.holdings.first[:quantity]}.by(update_params[:quantity])
+      end
+
+      it 'HTTPステータスが200であること' do
+        put "#{base_url}#{user[:id]}/holdings/#{user.holdings.first[:id]}", headers: tokens, params: update_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context '異常' do
+      context 'quantityが未入力の場合' do
+        let(:update_params) { { quantity: nil } }
+        # サインイン
+        before { post 'http://localhost:3000/api/auth', params: params }
+
+        it '更新できないこと' do
+          put "#{base_url}#{user[:id]}/holdings/#{user.holdings.first[:id]}", headers: tokens, params: update_params
+          expect(JSON.parse(response.body)['errors']).to eq('You must fill out the fields!!')
+        end
+
+        it 'HTTPステータスが422である(処理ができない)こと' do
+          put "#{base_url}#{user[:id]}/holdings/#{user.holdings.first[:id]}", headers: tokens, params: update_params
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+
+      context 'サインイン出来ていない場合' do
+        let(:update_params) { { quantity: 2 } }
+        before { put "#{base_url}#{user[:id]}/holdings/#{user.holdings.first[:id]}", params: update_params }
+
+        it '登録できないこと' do
+          expect(JSON.parse(response.body)['errors']).to eq(["You need to sign in or sign up before continuing."])
+        end
+
+        it 'HTTPステータスが401である(アクセス権がない)こと' do
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
+    end
+  end
+
   describe '#delete Action'
 end
